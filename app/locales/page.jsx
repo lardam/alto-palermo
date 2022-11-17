7fc1cc7c62360e9bf9469005ab4521a6b6720b27
lardam <felicitasgoyaprieto@airdefelicitas.home>
2022-11-17 00:38:16 -0300

    json tests - v0.32

--- app/locales/page.jsx
+++ app/locales/page.jsx
@@ -1,9 +1,22 @@
-export default function Stores(){
+const fetchStores = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+}
+
+export default async function Stores({ params }){
+    const stores = await fetchStores()
+
     return(
         <main id="main-stores">
             <h1 id="stores-title">locales</h1>
             <div id="stores-container">
-
+                <div id="stores-grid">
+                    {stores.map(store => (
+                        <>
+                            <p>{store.title}</p>
+                        </>
+                    ))}
+                </div>
             </div>
         </main>
     )
